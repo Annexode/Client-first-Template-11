01c56b849eed9c2e231165b72a961879787880b4
Annexode <annexode@gmail.com>
2024-10-17 22:15:07 +0200

    Homapage / Draft

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,21 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
-      fontFamily:{
-        robboto:["Roboto"]
+      fontFamily: {
+        robboto: ["Roboto"],
+      },
+      backgroundImage: {
+        blurGradient: "url('/assets/blurGradient.svg')",
+        blurGradientFooter: "url('/assets/blurGradientfooter.svg')",
+      },
+      colors:{
+        primary: "#212121",
+        secondary: "#F5F5F5",
+        tertiary: "#F9C23C",
+        accent: "#E91E63",
+        gradient: "linear-gradient(to right, #373FFF, #3ACAF8)"
       }
     },
   },
   plugins: [],
-}
-
+};
